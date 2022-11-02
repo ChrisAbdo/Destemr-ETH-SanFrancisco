5cfd76678742e7a89eb13819b6d02195009bcbde
chris abdo <chris.j.abdo@gmail.com>
2022-11-02 02:02:01 -0400

    init

--- components/APIKeyForm.tsx
+++ components/APIKeyForm.tsx
@@ -26,68 +26,61 @@ const APIKeyForm: React.FC<Props> = ({ setApiKey }) => {
   };
 
   return (
-    <form
-      className="px-4 h-3/5 flex justify-center flex-col lg:max-w-screen-md m-auto"
-      onSubmit={submitHandler}
-    >
-      <label htmlFor="apiKey">Livepeer.com:</label>
-      <input
-        type="text"
-        placeholder="Enter your api key"
-        className="border active:border-livepeer p-2 w-full rounded mb-8"
-        name="apiKey"
-        required
-      />
-
-      <label htmlFor="apiKey">JWPlayer:</label>
-      <input
-        type="text"
-        placeholder="Enter JWPlayer API Key"
-        className="border active:border-livepeer p-2 w-full rounded mb-2"
-        name="jwPlayerAPIKey"
-        required
-      />
-      <input
-        type="text"
-        placeholder="Enter JWPlayer API Secret"
-        className="border active:border-livepeer p-2 w-full rounded mb-4"
-        name="jwPlayerSecret"
-        required
-      />
-
-      <label>Video Title:</label>
-      <input
-        type="text"
-        placeholder="Enter video title"
-        className="border active:border-livepeer p-2 w-full rounded mb-4"
-        name="streamTitle"
-        defaultValue="Livepeer Demo"
-      />
-      <button
-        type="submit"
-        className="border border-1 border-black rounded px-4 py-2 lg:w-24 mx-auto"
+    <div>
+      <form
+        className="px-4 h-3/5 flex justify-center flex-col lg:max-w-screen-md m-auto"
+        onSubmit={submitHandler}
       >
-        Submit
-      </button>
-      <div className="pt-6 pb-2 text-sm text-gray-800">
-        Find out how to get your Livepeer.com API key{" "}
-        <a
-          href="https://livepeer.com/docs/guides/api-keys/create-an-api-key"
-          className="text-blue-500 underline"
+        <div className="flex flex-col border-[2px] border-black py-2 text-center">
+          <h1 className="font-bold underline text-xl">For Demo Purposes:</h1>
+          <p className="font-bold">
+            LivePeer API Key: 9fcff099-e211-4847-8e62-a85dd15c1506
+          </p>
+          <p className="font-bold">JWPlayer API Key: iSIdxRsD</p>
+          <p className="font-bold">JWPlayer Secret: Rn9V4gVAxsTlJ5LOEjAPUMvY</p>
+        </div>
+        <br />
+        <label htmlFor="apiKey">Livepeer.com API KEY:</label>
+        <input
+          type="text"
+          placeholder="Enter your api key"
+          className="border border-black active:border-livepeer p-2 w-full rounded mb-8 input input-bordered"
+          name="apiKey"
+          required
+        />
+
+        <label htmlFor="apiKey">JWPlayer:</label>
+        <input
+          type="text"
+          placeholder="Enter JWPlayer API Key"
+          className="border border-black active:border-livepeer p-2 w-full rounded mb-2 input input-bordered"
+          name="jwPlayerAPIKey"
+          required
+        />
+        <input
+          type="text"
+          placeholder="Enter JWPlayer API Secret"
+          className="border border-black active:border-livepeer p-2 w-full rounded mb-4 input input-bordered"
+          name="jwPlayerSecret"
+          required
+        />
+
+        <label>Video Title:</label>
+        <input
+          type="text"
+          placeholder="Enter video title"
+          className="border border-black active:border-livepeer p-2 w-full rounded mb-4 input input-bordered"
+          name="streamTitle"
+          defaultValue="Livepeer Demo"
+        />
+        <button
+          type="submit"
+          className="btn btn-ghost border border-1 border-black rounded px-4 py-2 lg:w-24 mx-auto"
         >
-          here
-        </a>
-      </div>
-      <div className="pb-6 text-sm text-gray-800">
-        Learn more about the JWPlayer API{" "}
-        <a
-          href="https://developer.jwplayer.com/jwplayer/docs/getting-started-with-content-management"
-          className="text-red-500 underline"
-        >
-          here
-        </a>
-      </div>
-    </form>
+          Start!
+        </button>
+      </form>
+    </div>
   );
 };
 
