88a9729066f4685b9dc9403e25581700439e5849
chris abdo <chris.j.abdo@gmail.com>
2022-11-02 03:31:28 -0400

    ADDED: Fixes to live streaming UI

--- components/APIKeyForm.tsx
+++ components/APIKeyForm.tsx
@@ -73,12 +73,23 @@ const APIKeyForm: React.FC<Props> = ({ setApiKey }) => {
           name="streamTitle"
           defaultValue="Livepeer Demo"
         />
-        <button
+        {/* <button
           type="submit"
           className="btn btn-primary border border-1 border-black rounded px-4 py-2 lg:w-24 mx-auto 
           tracking-wider"
         >
           Start!
+        </button> */}
+        <button
+          type="submit"
+          className="relative inline-block px-4 py-2  group"
+        >
+          <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-[#6AB313] group-hover:-translate-x-0 group-hover:-translate-y-0 border-black border-[2px]"></span>
+          <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-[#6AB313]"></span>
+
+          <span className="relative text-black group-hover:text-black ">
+            Go Live!
+          </span>
         </button>
       </form>
     </div>
